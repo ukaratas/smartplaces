2867344f9b01924c27c34fbc90b6c7197550c005
ukaratas <ukaratas@rocketmail.com>
2020-03-09 21:40:43 +0300

    Initialization

---
+++ sp.iot.server/sp.iot.server/Controllers/TankController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace sp.iot.server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TankController : ControllerBase
+    {
+
+        private readonly ILogger<TankController> _logger;
+
+        public TankController(ILogger<TankController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Tank> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Tank
+            {
+                Id = Guid.NewGuid(),
+                StartDate = DateTime.Now.AddDays(-index),
+                FinishDate = DateTime.Now,
+                Consumption = 55.5,
+            })
+            .ToArray();
+        }
+    }
+}
