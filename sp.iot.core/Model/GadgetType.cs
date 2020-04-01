47bf705cf0cdb1ac58a1b4a529438c6c117792cf
Uğur Karataş <ukaratas@rocketmail.com>
2020-04-01 17:26:52 +0300

    initial data setup

--- sp.iot.core/Model/GadgetType.cs
+++ sp.iot.core/Model/GadgetType.cs
@@ -11,8 +11,8 @@ namespace sp.iot.core
         Switch = 4000,
         Relay = 5000,
         Current = 6000,
-
-        Sensor = 9000
+        Sensor = 9000,
+        Other = 10000,
     }
 
     public enum GadgetType
@@ -22,11 +22,18 @@ namespace sp.iot.core
         LevelDigitalGeneric = 1002,
         LiquidFlowMeterYF06 = 2001,
         ValveNormallyClosed = 3001,
-        SwitchPush = 4001,
+        SwitchForLight = 4001,
+        SwitchForDevice = 4002,
         RelayForLight = 5001,
-        RelayForSocket = 5002,
+        RelayForOutlet = 5002,
         RelayForDevice = 5003,
-        SensorAirDTH22Heat = 9001,
-        SensorAirDTH22Humidity = 9002,
+        RelayForAction = 5004,
+        SensorAirSHT30Heat = 9001,
+        SensorAirSHT30Humidity = 9002,
+        SensorGasMQ6 = 9003,
+        SensorAirBME280Temprature = 9021,
+        SensorAirBME280Humidity = 9022,
+        SensorAirBME280Pressure = 9023,
+        OtherBuzzer = 10001
     }
 }
--- sp.iot.core/Model/UnitType.cs
+++ sp.iot.core/Model/UnitType.cs
@@ -5,9 +5,11 @@ namespace sp.iot.core
     public enum UnitType
     {
         NotSet = 0,
-        Percentage = 1001, 
+        Percentage = 1001,
         State = 1002,
-        Celcius = 1003, 
+        PPM = 1003,
+        Celcius = 1004,
+        HPA = 1005,
         LiquidLiter = 2001,
         LiquidGallon = 2002,
     }
